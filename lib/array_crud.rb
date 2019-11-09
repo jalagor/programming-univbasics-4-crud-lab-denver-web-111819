2f74601dd0b8bf98415e4d8c63022b9f67706ac3
John Rogala <johnrossrogala@gmail.com>
2019-11-09 01:17:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -38,8 +38,9 @@ def retrieve_element_from_index(array, index_number)
   
 end
 
-def retrieve_first_element_from_array(array)
-  
+def retrieve_first_element_from_array(array) 
+  smooth = [1, 2, 3, 4, 5]
+  smooth[1]
 end
 
 def retrieve_last_element_from_array(array)
