d87e3bbc0cb94cd260eca765920ed89bde0bbdcc
John Rogala <johnrossrogala@gmail.com>
2019-11-09 01:13:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,24 +1,34 @@
 def create_an_empty_array
-  
+  array = []
 end
 
 def create_an_array
+  arrays = [4, 5, 6, 7]
   
 end
 
 def add_element_to_end_of_array(array, element)
+ number_poop = [1, 2, 50, 80, 70]
+ number_poop.push("arrays!")
+ 
   
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  smack = ["smooch", "toot", "foot", "boot"]
+  smack.unshift("wow")
 end
 
-def remove_element_from_end_of_array(array)
+def remove_element_from_end_of_array(array) 
+  doot = ["ten", "third", "arrays!"] 
+  second = doot.pop
+ 
   
 end
 
 def remove_element_from_start_of_array(array)
+  loot = ["wow", 6, 7, 8, 9] 
+  loot.shift 
   
 end
 
